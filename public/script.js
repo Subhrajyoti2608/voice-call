d30b08fabae5b4a205b5048609c0180ab996056c
Subhrajyoti Mahato <86191223+Subhrajyoti2608@users.noreply.github.com>
2023-08-27 12:57:21 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -10,10 +10,29 @@ const myVideo = document.createElement("video");
 myVideo.muted = true;
 
 let myStream;
-navigator.mediaDevices.getUserMedia({video:true, audio:true}).then((stream)=>{
-    myStream = stream
-    addVideoStream(myVideo, stream)
-})
+
+navigator.mediaDevices
+    .getUserMedia({
+        audio: true,
+        video: false,
+    })
+    .then((stream) => {
+        myStream = stream;
+        addVideoStream(myVideo, stream);
+
+        socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
+        });
+    })
+
+function connectToNewUser(userId, stream) {
+    const call = peer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
+};
+
 function addVideoStream(video, stream) {
     video.srcObject = stream;
     video.addEventListener("loadedmetadata", () => {
@@ -47,3 +66,11 @@ $(function () {
 peer.on("open", (id) => {
     socket.emit("join-room", ROOM_ID, id);
 });
+
+peer.on("call",(call)=>{
+    call.answer(stream)
+    const video = document.createElement("video")
+    call.on("stream",(userVideoStream)=>{
+        addVideoStream(video, userVideoStream)
+    })
+})
